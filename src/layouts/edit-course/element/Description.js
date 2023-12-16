5304d0c31b56d687306102658f540cc487174762
Anushtha <kumarsen.bash@gmail.com>
2023-12-16 23:12:13 +0530

    description state

--- src/layouts/edit-course/element/Description.js
+++ src/layouts/edit-course/element/Description.js
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState } from "draft-js";
+import { EditorState, convertToRaw, ContentState, convertFromHTML } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+import draftToHtml from "draftjs-to-html";
 
 import Box from "@mui/material/Box";
 
-import { useModuleBuilderState } from "layouts/edit-course/ModuleBuilderState.provider";
+import {
+  useModuleBuilderState,
+  useChapterElementKeyValue,
+} from "layouts/edit-course/ModuleBuilderState.provider";
 
 const Description = ({ chapterIndex, element_id, ...rest }) => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
-  const { editChapterElement, data } = useModuleBuilderState();
-  const [editorState, seteditorState] = useState(EditorState.createEmpty());
+  const { editChapterElement } = useModuleBuilderState();
+  const value = useChapterElementKeyValue(chapterIndex, element_id, "value");
 
-  // const value = useChapterElementKeyValue(chapterIndex, element_id, "value");
+  const [editorState, seteditorState] = useState(() => {
+    // Parse HTML string and create ContentState
+    const htmlString = value ? value : "<p>Your HTML content goes here</p>";
+    const blocksFromHTML = convertFromHTML(htmlString);
+    const contentState = ContentState.createFromBlockArray(
+      blocksFromHTML.contentBlocks,
+      blocksFromHTML.entityMap
+    );
 
-  // const onChangeHandler = (e) => {
-  //   editChapterElement(chapterIndex, element_id, "value", e.target.value);
-  // };
+    return EditorState.createWithContent(contentState);
+  });
+
+  const handleBlur = () => {
+    const convertedValue = draftToHtml(convertToRaw(editorState.getCurrentContent()));
+    editChapterElement(chapterIndex, element_id, "value", convertedValue);
+  };
 
   const onEditorStateChange = (es) => {
     seteditorState(es);
@@ -37,7 +53,9 @@ const Description = ({ chapterIndex, element_id, ...rest }) => {
           padding: "4px 6px",
         }}
         initialEditorState={editorState}
+        editorState={editorState}
         onEditorStateChange={onEditorStateChange}
+        onBlur={handleBlur}
       />
     </Box>
   );
